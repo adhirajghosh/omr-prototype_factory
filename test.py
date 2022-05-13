a529496495550a60a8ccdafe3953376fb5b0e5b4
raember <raember@users.noreply.github.com>
2022-05-13 17:59:34 +0200

    Added additional test, added image folder and fixed glyph loading

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 import math
+from io import BytesIO
 
-from PIL import Image
+from PIL import Image as PImage
+from PIL.Image import Image
 
 # https://www.geogebra.org/classic
 # ggbApplet.getXcoord('H').toFixed() + ", " + -ggbApplet.getYcoord('H').toFixed() + ", " + Math.min(ggbApplet.getValue('l1').toFixed(), ggbApplet.getValue('l2').toFixed()) + ", " + Math.max(ggbApplet.getValue('l1').toFixed(), ggbApplet.getValue('l2').toFixed()) + ", " + (ggbApplet.getValue('α')*180/Math.PI).toFixed()
 from render import Render
 
 SAMPLES = [
-    ('data/ds2_dense/images/lg-2267728-aug-gutenberg1939--page-2.png', [
+    ('images/lg-2267728-aug-gutenberg1939--page-2.png', [
         {
             'proposal': (1475, 2379, 19, 26, 2 / 180.0 * math.pi),
             'class': 29,  # noteheadHalfOnLine
@@ -19,47 +21,50 @@ SAMPLES = [
             'gt': (127, 164, 44, 101, 5 / 180.0 * math.pi)
         }
     ]),
-    ('data/ds2_dense/images/lg-252689430529936624-aug-beethoven--page-3.png', [
+    ('images/lg-252689430529936624-aug-beethoven--page-3.png', [
         {
             'proposal': (506, 568, 16, 152, 0),
             'class': 123,  # tie
             'gt': (507, 569, 13, 148, 0)
         },
         {
-            'proposal': (),
+            'proposal': (277, 525, 20, 47, 2),
             'class': 64,  # accidentalSharp
-            'gt': ()
+            'gt': (273, 528, 18, 48, 0)
         }
     ])
 ]
 
-MASKS = {
-    6: Render(class_name='clefG', height=101, width=44, csv_path='name_uni.csv').render('Bravura.svg', save_svg=False,
-                                                                                        save_png=False),
-    29: Render(class_name='noteheadHalfOnLine', height=101, width=44, csv_path='name_uni.csv').render('Bravura.svg',
-                                                                                                      save_svg=False,
-                                                                                                      save_png=False),
-    64: Render(class_name='accidentalSharp', height=101, width=44, csv_path='name_uni.csv').render('Bravura.svg',
-                                                                                                   save_svg=False,
-                                                                                                   save_png=False),
-    123: Render(class_name='tie', height=101, width=44, csv_path='name_uni.csv').render('Bravura.svg', save_svg=False,
-                                                                                        save_png=False),
+
+def get_png(class_name: str, width: int, height: int) -> Image:
+    png_data = Render(
+        class_name=class_name, height=height, width=width, csv_path='name_uni.csv').render(
+        'Bravura.svg', save_svg=False, save_png=False)
+    with BytesIO(png_data) as bio:
+        return PImage.open(bio)
+
+
+GLYPHS = {
+    6: get_png('clefG', 44, 101),
+    29: get_png('noteheadHalfOnLine', 25, 16),
+    64: get_png('accidentalSharp', 30, 50),
+    123: get_png('tie', 148, 13),
 }
 
 
-def process(img, bbox, mask):
+def process(img, bbox, glyph):
     return bbox
 
 
-def calc_loss(img, bbox, mask) -> float:
+def calc_loss(img, bbox, glyph) -> float:
     pass
 
 
 if __name__ == '__main__':
     for image_fp, samples in SAMPLES:
-        img = Image.open(image_fp)
+        img = PImage.open(image_fp)
         for sample in samples:
-            mask = MASKS[sample['class']]
-            corr = process(img, sample['proposal'], mask)
-            gt_loss = calc_loss(img, sample['gt'], mask)
-            new_loss = calc_loss(img, sample['proposal'], mask)
+            glyph = GLYPHS[sample['class']]
+            corr = process(img, sample['proposal'], glyph)
+            gt_loss = calc_loss(img, sample['gt'], glyph)
+            new_loss = calc_loss(img, sample['proposal'], glyph)
