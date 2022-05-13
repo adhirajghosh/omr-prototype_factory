73565af81aa095ba6d3abe92990db53c4fb751df
Adhiraj Ghosh <adhirajghosh1998@gmail.com>
2022-05-13 12:24:54 +0200

    Exceptions added for tie, slur and tupletBracket

--- render.py
+++ render.py
@@ -65,7 +65,14 @@ class Render():
         glyph_names = [glyph.attributes["glyph-name"].value for glyph in glyphs]
         index_uni = glyph_names.index(uni_dict[self.class_name])
         base_path = glyphs[index_uni].attributes['d'].value
-        #TODO: Have new base_path for tie, beam, slur and tupletBracket
+        if self.class_name is 'tupletBracket':
+            self.class_name = 'beam'
+        if self.class_name is 'tie':
+            base_path = "m 58.172768,0.3197629 c -8.621801,7.1639345 -49.2377949,7.1639345 -57.85316243,0 v 0 c 8.61536753,6.1963135 49.23136143,6.1963135 57.85316243,0 z"
+        elif self.class_name is 'slur':
+            base_path = "m 154,141.7 1.2,1.3 C 140,155 114.7,158 95.8,158 64.5,158 49.6,150.8 40,143.3 l 1.4,-1.9 c 6.7,7.3 33.4,11.7 55.3,11.7 25.2,0 41.9,-2.8 57.3,-11.3 z"
+        else:
+            base_path = glyphs[index_uni].attributes['d'].value
 
         path_alt = parse_path(base_path)
         bbox = path_alt.bbox()
